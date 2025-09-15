e57a0c956d69729c96387a806af1d8b233d8791d
Rishu Singh <rishusingh@Rishus-MacBook-Air.local>
2025-09-15 12:19:51 +0530

    removed boilerplate

--- src/TicTactToe/Main.java
+++ src/TicTactToe/Main.java
@@ -1,11 +1,7 @@
 package TicTactToe;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
         Player player1 = new Player("Player1", Symbol.X);
         Player player2 = new Player("Player2", Symbol.O);
